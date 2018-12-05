7502e27619320856a37b3c6244ff60e7bbf76ace
Alexandre FERREIRA <alexandre.ferreira@axa.fr>
2018-12-05 15:02:49 +0100

    refacto

--- feature/GestionDesVirements.feature.spec.js
+++ feature/GestionDesVirements.feature.spec.js
@@ -19,86 +19,102 @@ defineFeature(feature, test => {
         serviceVirement = new ServiceVirement();
     })
 
-    test('Virement simple', ({ given, when, then, pending }) => {
+    const givenJaiUnCompteChequeAvecUnSoldeDeX = given => {
         given(/^j'ai un compte cheque avec un solde de (.*)€$/, (solde) => {
             compteCheque.setSolde(parseInt(solde))
         });
+    }
 
+    const givenJaiUnCompteEpargneAvecUnSoldeDeX = given => {
         given(/^j'ai un compte épargne avec un solde de (.*)€$/, (solde) => {
             compteEpargne.setSolde(parseInt(solde))
         });
+    }
 
-        when(/^j'effectue un virement de (.*)€ du compte cheque vers le compte épargne$/, (virement) => {
-            statusVirement = serviceVirement.effectuerVirement(virement, compteCheque, compteEpargne)
-        });
-
-        then(/^le solde du comte cheque est de (.*)€$/, (nouveauSolde) => {
-            expect(compteCheque.solde).toBe(parseInt(nouveauSolde))
-        });
-
-        then(/^le solde du compte épargne est de (.*)€$/, (nouveauSolde) => {
-            expect(compteEpargne.solde).toBe(parseInt(nouveauSolde))
-        });
-
-        then(/^le virement est accepté$/, () => {
-            expect(statusVirement).toBe(VIREMENT_STATUS.ACCEPTE)
-        })
-    });
-
-    test('Virement hors provision', ({ given, when, then, pending }) => {
-        given(/^j'ai un compte cheque avec un solde de (.*)€$/, (solde) => {
-            compteCheque.setSolde(parseInt(solde))
-        });
-
-        given(/^j'ai un compte épargne avec un solde de (.*)€$/, (solde) => {
-            compteEpargne.setSolde(parseInt(solde))
-        });
-
-        when(/^j'effectue un virement de (.*)€ du compte cheque vers le compte épargne$/, (virement) => {
-            statusVirement = serviceVirement.effectuerVirement(virement, compteCheque, compteEpargne)
-        });
-
-        then(/^le solde du compte cheque est de (.*)€$/, (nouveauSolde) => {
-            expect(compteCheque.solde).toBe(parseInt(nouveauSolde))
-        });
-
-        then(/^le solde du compte épargne est de (.*)€$/, (nouveauSolde) => {
-            expect(compteEpargne.solde).toBe(parseInt(nouveauSolde))
-        });
-
-        then('le virement est réfusé motif provision insuffisante', () => {
-            expect(statusVirement).toBe(VIREMENT_STATUS.REFUSE_SOLDE_INSUFFISANT)
-        });
-    });
-
-    test('Virement plafonné', ({ given, when, then, pending }) => {
-        given(/^j'ai un compte cheque avec un solde de (.*)€$/, (solde) => {
-            compteCheque.setSolde(parseInt(solde))
-        });
-
-        given(/^j'ai un compte épargne avec un solde de (.*)€$/, (solde) => {
-            compteEpargne.setSolde(parseInt(solde))
-        });
-
+    const givenJaiUnPlafondSurLeCompteChequeDeX = given => {
         given(/^j'ai un plafond sur le compte cheque de (.*)€$/, (plafond) => {
             compteCheque.setPlafond(parseInt(plafond))
         });
+    }
 
+    const whenJEffectueUnVirementDeXDuCompteChequeVersLeCompteEpargne = when => {
         when(/^j'effectue un virement de (.*)€ du compte cheque vers le compte épargne$/, (virement) => {
             statusVirement = serviceVirement.effectuerVirement(virement, compteCheque, compteEpargne)
         });
+    }
 
+    const thenLeSoldeDuCompteChequeEstDeX = then => {
         then(/^le solde du compte cheque est de (.*)€$/, (nouveauSolde) => {
             expect(compteCheque.solde).toBe(parseInt(nouveauSolde))
         });
+    }
 
+    const thenLeSoldeDuCompteEpargneEstDeX = then => {
         then(/^le solde du compte épargne est de (.*)€$/, (nouveauSolde) => {
             expect(compteEpargne.solde).toBe(parseInt(nouveauSolde))
         });
+    }
 
+    const thenLeVirementEstAccepte = then => {
+        then(/^le virement est accepté$/, () => {
+            expect(statusVirement).toBe(VIREMENT_STATUS.ACCEPTE)
+        })
+    }
+
+    const thenLeVirementEstRefuseMotifSoldeInsiffisant = then => {
+        then('le virement est réfusé motif provision insuffisante', () => {
+            expect(statusVirement).toBe(VIREMENT_STATUS.REFUSE_SOLDE_INSUFFISANT)
+        });
+    }
+
+    const thenLeVirementEstRefuseMotifPlafondDepasse = then => {
         then('le virement est réfusé motif plafond dépassé', () => {
             expect(statusVirement).toBe(VIREMENT_STATUS.REFUSE_PLAFOND_DEPASSE)
         });
+    }
+
+    test('Virement simple', ({ given, when, then, pending }) => {
+        givenJaiUnCompteChequeAvecUnSoldeDeX(given);
+
+        givenJaiUnCompteEpargneAvecUnSoldeDeX(given);
+
+        whenJEffectueUnVirementDeXDuCompteChequeVersLeCompteEpargne(when);
+
+        thenLeSoldeDuCompteChequeEstDeX(then);
+
+        thenLeSoldeDuCompteEpargneEstDeX(then);
+
+        thenLeVirementEstAccepte(then);
+    });
+
+    test('Virement hors provision', ({ given, when, then, pending }) => {
+        givenJaiUnCompteChequeAvecUnSoldeDeX(given);
+
+        givenJaiUnCompteEpargneAvecUnSoldeDeX(given);
+
+        whenJEffectueUnVirementDeXDuCompteChequeVersLeCompteEpargne(when);
+
+        thenLeSoldeDuCompteChequeEstDeX(then);
+
+        thenLeSoldeDuCompteEpargneEstDeX(then);
+
+        thenLeVirementEstRefuseMotifSoldeInsiffisant(then);
+    });
+
+    test('Virement plafonné', ({ given, when, then, pending }) => {
+        givenJaiUnCompteChequeAvecUnSoldeDeX(given);
+
+        givenJaiUnCompteEpargneAvecUnSoldeDeX(given);
+
+        givenJaiUnPlafondSurLeCompteChequeDeX(given);
+
+        whenJEffectueUnVirementDeXDuCompteChequeVersLeCompteEpargne(when);
+
+        thenLeSoldeDuCompteChequeEstDeX(then);
+
+        thenLeSoldeDuCompteEpargneEstDeX(then);
+
+        thenLeVirementEstRefuseMotifPlafondDepasse(then);
     });
 
 
